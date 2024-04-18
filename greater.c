edb28c108934615ad1d92df186640c255ea6c55f
Deeksha380824 <deekshabudhlakoti44@gmail.com>
2024-04-18 13:09:08 +0530

    new

---
+++ greater.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(a>b)
+    {
+        printf("a is greater");
+    }
+    else
+    {
+        printf("b id greater");
+    }
+    return 0;
+}
