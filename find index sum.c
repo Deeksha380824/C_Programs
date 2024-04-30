24a0711de86fd36f9fa37b2682065b7c1d6f4994
Deeksha380824 <134601127+Deeksha380824@users.noreply.github.com>
2024-04-30 18:16:03 +0530

    Update find index sum.c

---
+++ find index sum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int arr[5]={1,2,3,4,5};
+	int i;
+	int j;
+	for(i=0;i<=5;i++)
+	{
+		for(j=i+1;j<5;j++)
+		{
+			if(arr[i]+arr[j]==5)
+			{
+				printf("\n true");
+				break;
+			}
+			else
+			{
+				printf("\nfalse");
+			}
+		}
+		
+	}
+}
